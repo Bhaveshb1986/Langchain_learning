4280f45c8fac9b642e5ddbccd44de330fd7147df
Bhavesh <bhatnagar.bhavesh@gmail.com>
2025-12-27 09:28:59 +0530

    envirnment setup

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from dotenv import load_dotenv
+
+# import os
+
+# Load the .env file
+load_dotenv()
+
+
 def main():
     print("Hello from new-a!")
+
+
+#    print("API_KEY", os.getenv("OPEN_API_KEY"))
 
 
 if __name__ == "__main__":
